107571a58f18b41a3fed6f5d8c92be369841cd85
Godfrey <godfreyjr@gmail.com>
2017-07-09 21:19:15 -0500

    Add code to check if user exists and buttons to filter user status

--- twitch.js
+++ twitch.js
@@ -4,31 +4,70 @@ var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", 'mr_mamma
 var elementUl = document.getElementById('results');
 
 userNames.forEach(function(name) {
-  var url = 'https://wind-bow.gomix.me/twitch-api/streams/' + name + '?callback=?'
+  var urlChannel = 'https://wind-bow.gomix.me/twitch-api/channels/' + name + '?callback=?'
+  var urlStream = 'https://wind-bow.gomix.me/twitch-api/streams/' + name + '?callback=?'
   var link = link = 'https://www.twitch.tv/' + name;
-  var detail, status;
+  var message;
 
-  $.getJSON(url, function(data) {
-    if (data.stream !== null) {
-      detail = data.stream.channel.status;
-      status = 'online'
-    }
-    else {
-      detail = 'Currently offline';
-      status = 'offline';
-    }
+  $.getJSON(urlChannel, function(channelObj) {
 
-    var userLi = document.createElement('li');
-    userLi.innerHTML = '<h3><a href ='+ link +' target = blank>'+ name +'</a></h3>' +
-                        '<p>'+ detail +'</p>';
+    $.getJSON(urlStream, function(streamObj) {
 
-    elementUl.appendChild(userLi);
+      var userLi = document.createElement('li');
+
+      if (channelObj.error) {
+        message = 'Account Does Not Exist';
+        userLi.className = 'not found';
+      }
+      else if (streamObj.stream) {
+        message = streamObj.stream.channel.status;
+        userLi.className = 'online';
+      }
+      else {
+        message = 'Currently offline';
+        userLi.className = 'offline';
+      }
+
+      userLi.innerHTML = '<h3><a href ='+ link +' target = blank>'+ name +'</a></h3>' +
+                          '<p>'+ message +'</p>';
+
+      elementUl.appendChild(userLi);
+    });
 
   });
+
 });
 
-var footer = document.getElementById('footer');
-footer.style.position = 'relative';
+var allUsers = document.getElementsByTagName('LI');  //returns HTMLCollection, not an array
 
+function showOnline() {
+  for(var i = 0; i < allUsers.length; i++) {
+    allUsers[i].style.display = 'list-item';
+    if(allUsers[i].className !== 'online')
+      allUsers[i].style.display = 'none';
+  }
+}
 
+function showOffline() {
+  for(var i = 0; i < allUsers.length; i++) {
+    allUsers[i].style.display = 'list-item';
+    if(allUsers[i].className !== 'offline')
+      allUsers[i].style.display = 'none';
+  }
+}
 
+function showNotFound() {
+  for(var i = 0; i < allUsers.length; i++) {
+    allUsers[i].style.display = 'list-item';
+    if(allUsers[i].className !== 'not found')
+      allUsers[i].style.display = 'none';
+  }
+}
+
+function showAll() {
+  for(var i = 0; i < allUsers.length; i++) {
+    allUsers[i].style.display = 'list-item';
+  }
+}
+
+document.getElementById('footer').style.position = 'relative';
